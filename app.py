5aee9f6412e45d496da33438bb0daf6bc4ddbfa8
GitHub Pages Deployer <deploy@example.com>
2025-05-15 11:12:02 +1000

    Initial backend commit for Render deployment

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def apply_cors_headers(response):
         response.headers['Access-Control-Allow-Origin'] = origin
         response.headers['Access-Control-Allow-Credentials'] = 'true'
         response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS']
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
         response.headers['Vary'] = 'Origin'
     return response
 
